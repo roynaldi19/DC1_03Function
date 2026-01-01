b8228f003918f53d9ca0fcf7559dd6352aa65151
roynaldi19 <roynaldi19@gmail.com>
2026-01-01 16:12:44 +0700

    Initial commit

---
+++ src/Function.kt
@@ -0,0 +1,10 @@
+fun sum(valueA: Int, valueB: Int): Int {
+    val result = valueA + valueB
+    return result
+}
+
+fun main() {
+    val penjumlahan = sum(10, 23)
+    println("Hasil $penjumlahan")
+
+}
